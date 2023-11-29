ca0286141946d59e0127e22735090a8d551de78b
Racheline <136965489+Rachel2001@users.noreply.github.com>
2023-11-29 17:58:38 +0200

    Added elements and made more changes

---
+++ src/index.js
@@ -0,0 +1,26 @@
+function updateWeatherInfo(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = temperature.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+  
+}
+
+function searchCity(city) {
+    let apiKey = "de0c2ca662bo5051774fet6becf43f3b";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(updateWeatherInfo);
+}
+
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = searchInput.value
+    searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
